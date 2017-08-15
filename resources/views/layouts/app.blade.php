ce5fb808d71e1aa23635a52eb81eecbbc7cec8ba
begimov <begimov@gmail.com>
2017-08-15 10:36:42 +0300

    footer partial added

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,6 +8,7 @@
     @include('layouts.partials._header')
     @include('layouts.partials._navigation')
     @yield('content')
+    @include ('layouts.partials._footer')
   </div>
   <!-- Scripts -->
   <script src="{{ asset('js/app.js') }}"></script>
