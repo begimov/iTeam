b1e3ce663368d4d1c1f29e1bb2925a68030bf6d2
begimov <begimov@gmail.com>
2017-11-22 08:34:44 +0300

    bootstrap 4 fixes and new navigation bar layout created

--- resources/views/layouts/partials/_navigation.blade.php
+++ resources/views/layouts/partials/_navigation.blade.php
@@ -1,8 +1,41 @@
-<nav class="navbar navbar-default navbar-static-top">
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<a class="navbar-brand" href="{{ url('/') }}">{{ config('app.name', 'Laravel') }}</a>
+<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+  <span class="navbar-toggler-icon"></span>
+</button>
+
+<div class="collapse navbar-collapse" id="navbarSupportedContent">
+  <ul class="navbar-nav mr-auto">
+  @if (!Auth::guest())
+    <li class="nav-item">
+      <a class="nav-link" href="{{ route('login') }}">@lang('auth.login_btn')</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="{{ route('register') }}">@lang('auth.register_btn')</a>
+    </li>
+    @else
+    <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Dropdown
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+        <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();document.getElementById('logout-form').submit();">Logout</a>
+        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">{{ csrf_field() }}</form>
+          <a class="dropdown-item" href="#">Action</a>
+          <a class="dropdown-item" href="#">Another action</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">Something else here</a>
+        </div>
+      </li>
+    @endif
+  </ul>
+</div>
+</nav>
+
+<!-- <nav class="navbar navbar-default navbar-static-top">
     <div class="container">
         <div class="navbar-header">
 
-            <!-- Collapsed Hamburger -->
             <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
                 <span class="sr-only">Toggle Navigation</span>
                 <span class="icon-bar"></span>
@@ -10,21 +43,17 @@
                 <span class="icon-bar"></span>
             </button>
 
-            <!-- Branding Image -->
             {{-- <a class="navbar-brand" href="{{ url('/') }}">
                 {{ config('app.name', 'Laravel') }}
             </a> --}}
         </div>
 
         <div class="collapse navbar-collapse" id="app-navbar-collapse">
-            <!-- Left Side Of Navbar -->
             <ul class="nav navbar-nav">
                 <li><a href="#">//</a></li>
             </ul>
 
-            <!-- Right Side Of Navbar -->
             <ul class="nav navbar-nav navbar-right">
-                <!-- Authentication Links -->
                 @if (Auth::guest())
                     <li><a href="{{ route('login') }}">@lang('auth.login_btn')</a></li>
                     <li><a href="{{ route('register') }}">@lang('auth.register_btn')</a></li>
@@ -52,4 +81,4 @@
             </ul>
         </div>
     </div>
-</nav>
+</nav> -->
