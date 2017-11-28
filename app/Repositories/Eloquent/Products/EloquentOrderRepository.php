7b7b3caa0fd547efec3097be05333cd4d48b0733
begimov <begimov@gmail.com>
2017-11-28 13:09:16 +0300

    EloquentOrderRepository created

---
+++ app/Repositories/Eloquent/Products/EloquentOrderRepository.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Repositories\Eloquent\Products;
+
+use App\Repositories\EloquentRepositoryAbstract;
+use App\Repositories\Contracts\Products\OrderRepository;
+
+use App\Models\Products\Order;
+
+class EloquentOrderRepository extends EloquentRepositoryAbstract implements OrderRepository
+{
+    public function entity()
+    {
+        return Order::class;
+    }
+}
