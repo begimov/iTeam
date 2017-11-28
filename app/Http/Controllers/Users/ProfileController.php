e2d06b84a9abed05099bbb56539b072c4c478cad
begimov <begimov@gmail.com>
2017-11-28 13:34:31 +0300

    ProfileController created

---
+++ app/Http/Controllers/Users/ProfileController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Users;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class ProfileController extends Controller
+{
+    public function index()
+    {
+        echo 'sdfsdf';
+    }
+}
