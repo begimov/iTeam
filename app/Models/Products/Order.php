8b2f1b93dee1a7825d1f247e866c658714e486ff
begimov <begimov@gmail.com>
2017-11-29 13:21:19 +0300

    Order model now using SoftDeletes

--- app/Models/Products/Order.php
+++ app/Models/Products/Order.php
@@ -3,8 +3,16 @@
 namespace App\Models\Products;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Order extends Model
 {
-    //
+    use SoftDeletes;
+
+    /**
+     * The attributes that should be mutated to dates.
+     *
+     * @var array
+     */
+    protected $dates = ['deleted_at'];
 }
