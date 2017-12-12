3a5fbc5a76e50050a644fe7371f48c5f71548a15
begimov <begimov@gmail.com>
2017-12-12 12:49:26 +0300

    basic profile setting blade template implemented

--- resources/views/users/profile/settings.blade.php
+++ resources/views/users/profile/settings.blade.php
@@ -1,1 +1,35 @@
-SETTIGNS
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <form method="get" action="#">
+            <div class="row">
+                <div class="col-sm-6">
+                    <div class="form-group">
+                        <label for="email">Email</label>
+                        <input type="email" class="form-control" name="email" id="email">
+                    </div>
+                    <div class="form-group">
+                        <label for="name">Имя</label>
+                        <input type="text" class="form-control" name="name" id="name">
+                    </div>
+                    <div class="form-group">
+                        <label for="phone">Телефон</label>
+                        <input type="text" class="form-control" name="phone" id="phone">
+                    </div>
+                </div>
+                <div class="col-sm-6">
+                    <div class="form-group">
+                        <label for="company">Компания</label>
+                        <input type="text" class="form-control" name="company" id="company">
+                    </div>
+                    <div class="form-group">
+                        <label for="businessEntity">ОПФ</label>
+                        <input type="text" class="form-control" name="businessEntity" id="businessEntity">
+                    </div>
+                </div>
+            </div>
+            <button type="submit" class="btn btn-primary">Сохранить</button>
+        </form>
+    </div>
+@endsection
