497a139ecb539581e193fb658195d9ab81661c43
begimov <begimov@gmail.com>
2017-12-12 12:24:27 +0300

    profile settings option added to navigation menu

--- resources/views/layouts/partials/_navigation.blade.php
+++ resources/views/layouts/partials/_navigation.blade.php
@@ -26,6 +26,7 @@
         </a>
         <div class="dropdown-menu" aria-labelledby="navbarDropdown">
         <a class="dropdown-item" href="{{ route('user.dashboard.index') }}">Заказы</a>
+        <a class="dropdown-item" href="{{ route('user.profile.settings') }}">Профиль</a>
         <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();document.getElementById('logout-form').submit();">Logout</a>
         <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">{{ csrf_field() }}</form>
         </div>
