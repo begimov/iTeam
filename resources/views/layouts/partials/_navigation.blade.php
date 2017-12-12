41cb8fab6140392f69d1083b4c3864c2f90802df
begimov <begimov@gmail.com>
2017-12-12 13:32:29 +0300

    navigation partial updated

--- resources/views/layouts/partials/_navigation.blade.php
+++ resources/views/layouts/partials/_navigation.blade.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-<a class="navbar-brand" href="{{ url('/') }}">{{ config('app.name', 'Laravel') }}</a>
+<!-- <a class="navbar-brand" href="{{ url('/') }}">{{ config('app.name', 'Laravel') }}</a> -->
 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
   <span class="navbar-toggler-icon"></span>
 </button>
@@ -7,7 +7,17 @@
 <div class="collapse navbar-collapse" id="navbarSupportedContent">
 <ul class="navbar-nav mr-auto">
     <li class="nav-item">
-      <a class="nav-link" href="{{ route('login') }}">СТАТЬИ</a>
+      <a class="nav-link" href="#">Блог</a>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Мастер-проекты и мастер-классы
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">Мастер-проекты</a>
+        </div>
+      </li>
+      <a class="nav-link" href="#">Компания</a>
+      <a class="nav-link" href="#">Контакты</a>
     </li>
   </ul>
   
