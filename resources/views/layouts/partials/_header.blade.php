637b55513ea31fa0b24a63c0e5b394ff896db210
begimov <begimov@gmail.com>
2017-12-12 14:38:30 +0300

    basic logo added to header partial

--- resources/views/layouts/partials/_header.blade.php
+++ resources/views/layouts/partials/_header.blade.php
@@ -1,7 +1,23 @@
 <div class="container-fluid">
   <div class="row">
     <div class="col-md-12">
-      HEADER
+      <svg id="svg1" width="48" height="48" fill="#c00" stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 140 140" xmlns="http://www.w3.org/2000/svg">
+        <g id="svg1g0">
+          <circle id="svg1c0" r="20" cy="70" cx="70"/>
+        </g>
+        <g id="svg1g1">
+          <circle id="svg1c1" r="20" cy="20" cx="70"/>
+          <path id="svg1p1" fill="none" stroke="#c00" stroke-width="16" d="M36,34 A47,47 0 0 1 104,34" />
+        </g>
+        <g id="svg1g2" transform="rotate(120,70,70)">
+          <circle id="svg1c2" r="20" cy="20" cx="70"/>
+          <path id="svg1p2" fill="none" stroke="#c00" stroke-width="16" d="M36,34 A47,47 0 0 1 104,34" />
+        </g>
+        <g id="svg1g3" transform="rotate(-120,70,70)">
+          <circle id="svg1c3" r="20" cy="20" cx="70"/>
+          <path id="svg1p3" fill="none" stroke="#c00" stroke-width="16" d="M36,34 A47,47 0 0 1 104,34" />
+        </g>
+      </svg><a href="/">iTeam</a>
     </div>
   </div>
 </div>
