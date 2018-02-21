9333e94d7f43eb9049f770d26ddf3debbdc3bf1e
begimov <begimov@gmail.com>
2018-02-21 14:20:20 +0300

    test buy button for a get route added to index home template

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -13,12 +13,7 @@
             <h1>Заголовок</h1>
             <p class="lead">Подзаголовок</p>
             <p>Описание</p>
-            <form action="{{ route('orders.store') }}" method="post">
-              <input type="hidden" value="3" name="product_id">
-              <input type="hidden" value="33" name="price_tag">
-              {{ csrf_field() }}
-              <button type="submit">КУПИТЬ Продукт 1</button>
-            </form>
+            <p><a class="btn btn-primary btn-lg" href="{{ route('user.dashboard.index', ['product_id' => 3, 'price_tag' => 3]) }}" role="button">BUY</a></p>
             <p><a class="btn btn-primary btn-lg" href="#" role="button">CTA</a></p>
           </div>
         </div>
