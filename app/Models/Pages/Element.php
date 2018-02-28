d3f57f18600359bd53a5c0a7db51a635f12c5150
begimov <begimov@gmail.com>
2018-02-28 15:46:50 +0300

    Element model class created

---
+++ app/Models/Pages/Element.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models\Pages;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Represents Element of a Page using Block as view
+ */
+class Element extends Model
+{
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'data' => 'array',
+    ];
+    
+    public function block()
+    {
+        return $this->belongsTo(Block::class);
+    }
+}
