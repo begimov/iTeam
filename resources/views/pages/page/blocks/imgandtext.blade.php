03683faa665ea200c271b4b6705e456b58a03b14
begimov <begimov@gmail.com>
2018-03-14 08:57:06 +0300

    imgandtext block updated to get img filename

--- resources/views/pages/page/blocks/imgandtext.blade.php
+++ resources/views/pages/page/blocks/imgandtext.blade.php
@@ -2,7 +2,8 @@
   <div class="panel panel-default">
     <div class="panel-body">
       <strong>Image and text</strong>
-        <p>Путь: {{ $element->data['path'] }}</p>
+        <p>Путь: {{ $element->data['imgId'] }}</p>
+        <img src="{{ App\Models\Content\File::find($element->data['imgId'])->name }}">
         <p>Имя: {{ $element->data['name'] }}</p>
         <p>Ссылка: {{ $element->data['link'] }}</p>
     </div>
