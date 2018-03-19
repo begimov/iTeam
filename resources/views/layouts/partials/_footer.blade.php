985738c8c2031240a2ad5553dfbb86e1ce65c6af
begimov <begimov@gmail.com>
2018-03-19 08:58:47 +0300

    new footer layout added

--- resources/views/layouts/partials/_footer.blade.php
+++ resources/views/layouts/partials/_footer.blade.php
@@ -1,11 +1,47 @@
-<div class="container-fluid">
-  <div class="row footer">
-    <div class="col-md-12">
-      © 2002 - 2017 iTeam<br>
-      Москва, Пресненская наб. 12<br>
-
-      (499) 110-2684<br>
-      info@iteam.ru
+<!-- FOOTER -->
+<div class="container-fluid bg-dark text-white block-footer mt-5">
+    <div class="row p-5">
+        <div class="col-xl-3 col-md-6 col-sm-8">
+            <p><h5>&copy;&nbsp;2002&nbsp;&mdash; 2018 iTeam</h5></p>
+            <p>Москва, Пресненская наб.&nbsp;12</p>
+            <p><span class="lead">(499) 110-2684</span><br><a href="mailto:info@iteam.ru">info@iteam.ru</a></p>
+            <p>Все контакты</p>
+            <p>
+                <a href="">Пользовательское соглашение</a><br>
+                <a href="">Договор-оферта</a>
+            </p>
+        </div>
+        <div class="col-xl-2 col-md-3 col-sm-4">
+            <p><h5>Компания</h5></p>
+            <p>
+                <a href="">О компании</a><br>
+                <a href="">Услуги</a><br>
+                <a href="">Проекты</a><br>
+                <a href="">Отзывы</a>
+            </p>
+        </div>
+        <div class="col-xl-2 col-md-3">
+            <p><h5>Обучение</h5></p>
+            <p>
+                <a href="">Мастер-классы</a><br>
+                <a href="">Мастер-проекты</a>
+            </p>
+        </div>
+        <div class="col-xl-3 col-md-6 col-sm-8">
+            <p><h5>Соц. сети</h5></p>
+            <p>Подписывайтесь и&nbsp;читайте в&nbsp;социальных сетях</p>
+            <p>
+                <ul class="list-inline">
+                    <li class="list-inline-item"><a href="https://www.facebook.com/ITeam-Management-1538687959484919/"><img src="img/V2/sn_fb.png"></a></li>
+                    <li class="list-inline-item"><a href="https://vk.com/public150574411"><img src="img/V2/sn_vk.png"></a></li>
+                    <li class="list-inline-item"><a href="https://www.instagram.com/iteamanagement/"><img src="img/V2/sn_instag.png"></a></li>
+                </ul>
+            </p>
+        </div>
+        <div class="col-xl-2 col-md-6 col-sm-4">
+            <p><h5>&nbsp;</h5></p>
+            <p><a href="https://megastock.ru/" target="_blank"><img src="https://iteam.ru/img/wm.png" alt="Webmoney"></a></p>
+            <p><a href="http://wiki.webmoney.ru/projects/webmoney/wiki/Оплата_товаров_и_услуг_через_сервис_Мерчант" target="_blank">порядок оплаты</a></p>
+        </div>
     </div>
-  </div>
 </div>
