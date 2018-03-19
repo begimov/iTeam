59760c237c7ce52c46cfea2417ce819cc08f6631
begimov <begimov@gmail.com>
2018-03-19 10:20:16 +0300

    Roboto font pulled in

--- resources/views/layouts/partials/_head.blade.php
+++ resources/views/layouts/partials/_head.blade.php
@@ -3,6 +3,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <!-- CSRF Token -->
 <meta name="csrf-token" content="{{ csrf_token() }}">
+<link href="https://fonts.googleapis.com/css?family=Roboto:300,400,400i,900,900i&amp;subset=cyrillic" rel="stylesheet">
 <title>{{ config('app.name', 'Laravel') }}</title>
 <!-- Styles -->
 <link href="{{ asset('css/app.css') }}" rel="stylesheet">
