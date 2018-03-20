dfa716d42bb6bda4b959b4a9487ffbe8e29f5f65
begimov <begimov@gmail.com>
2018-03-20 17:56:51 +0300

    basic displaying of category pages implemented

--- resources/views/pages/category/index.blade.php
+++ resources/views/pages/category/index.blade.php
@@ -1,7 +1,18 @@
 @extends('layouts.app')
 
 @section('content')
-    @foreach ($category->pages as $page)
-        {{ $page->name }}
-    @endforeach
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <h1>{{ $category->name }}</h1>
+            </div>
+        </div>
+        @foreach ($category->pages as $page)
+            <div class="row">
+                <div class="col">
+                    <h3><a href="{{ route('pages.show', $page->id) }}">{{ $page->name }}</a></h3>
+                </div>
+            </div>
+        @endforeach
+    </div>
 @endsection
