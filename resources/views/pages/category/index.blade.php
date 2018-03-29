8ec06bc75ef47126de6f7961f735f42426af8699
begimov <begimov@gmail.com>
2018-03-29 15:38:08 +0300

    category template layout updated

--- resources/views/pages/category/index.blade.php
+++ resources/views/pages/category/index.blade.php
@@ -3,14 +3,19 @@
 @section('content')
     <div class="container">
         <div class="row">
-            <div class="col">
+            <div class="col mt-5 mb-4">
                 <h1>{{ $category->name }}</h1>
             </div>
         </div>
         @foreach ($category->pages as $page)
-            <div class="row">
+            <div class="row mb-3">
                 <div class="col">
-                    <h3><a href="{{ route('pages.show', $page->id) }}">{{ $page->name }}</a></h3>
+                    <div class="card">
+                        <div class="card-body">
+                            <h3><a href="{{ route('pages.show', $page->id) }}">{{ $page->name }}</a></h3>
+                            <p class="lead m-0">{{ $page->description }}</p>
+                        </div>
+                    </div>
                 </div>
             </div>
         @endforeach
