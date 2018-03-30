65fe6c4deebeaa0ddfc5283abf1c1f379f0d972a
begimov <begimov@gmail.com>
2018-03-30 11:34:36 +0300

    purchase block template updated to store new order

--- resources/views/pages/page/blocks/purchase.blade.php
+++ resources/views/pages/page/blocks/purchase.blade.php
@@ -1,7 +1,14 @@
 <div class="row">
-  <div class="col-md-12">
+  <div class="col">
+  <?php $elementsProduct = \App\Models\Products\Product::find($element->data['product']['productId']) ?>
+    <h2>{{ $element->data['product']['pricetagId'] ?: $elementsProduct->price }}</h2>
     <p>
-      <a 
+    <form action="{{ route('orders.store') }}" method="POST">
+      <input type="hidden" name="product_id" value="$element->data['product']['productId']">
+      <input type="hidden" name="price_tag_id" value="$element->data['product']['pricetagId']">
+      <button type="submit" class="btn btn-primary btn-lg">КУПИТЬ</button>
+    </form>
+      <!-- <a 
         class="btn btn-primary btn-lg"
         href="{{ route(
           'user.dashboard.index',
@@ -12,7 +19,7 @@
           )
         }}"
         role="button">
-        BUY</a>
+        КУПИТЬ</a> -->
     </p>
   </div>
 </div>
