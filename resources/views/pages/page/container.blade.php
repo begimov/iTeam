d684a59dc3404e934bae4fed8ec5148081c79ff2
begimov <begimov@gmail.com>
2018-03-30 10:50:28 +0300

    margin added on root container for page template

--- resources/views/pages/page/container.blade.php
+++ resources/views/pages/page/container.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+<div class="container my-5">
   @foreach ($page->elements as $element)
     @include('pages.page.blocks.' . $element->block->view)
   @endforeach
