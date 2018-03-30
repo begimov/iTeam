9d158d5d05ca658ecb144bedc2d1eac70fb95314
begimov <begimov@gmail.com>
2018-03-30 12:56:10 +0300

    unused orders repository dependency removed from Dashboard controller

--- app/Http/Controllers/Users/DashboardController.php
+++ app/Http/Controllers/Users/DashboardController.php
@@ -4,15 +4,9 @@ namespace App\Http\Controllers\Users;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-use App\Repositories\Contracts\Products\OrderRepository;
 
 class DashboardController extends Controller
 {
-    public function __construct(OrderRepository $orders)
-    {
-        $this->orders = $orders;
-    }
-
     public function index(Request $request)
     {
         return view('users.dashboard');
