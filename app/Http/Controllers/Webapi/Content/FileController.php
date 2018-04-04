50f1644e3af16b00677ef59822518d23aee96376
begimov <begimov@gmail.com>
2018-04-04 12:40:36 +0300

    file-material relationship method added to File class

--- app/Http/Controllers/Webapi/Content/FileController.php
+++ app/Http/Controllers/Webapi/Content/FileController.php
@@ -5,10 +5,14 @@ namespace App\Http\Controllers\Webapi\Content;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
+use App\Models\Content\File;
+
 class FileController extends Controller
 {
-    public function show($name)
+    public function show(File $file)
     {
-        dd($name);
+        // TODO: validation user paid for product which has current file in its materials
+        $path = storage_path('app/files/materials/id_' . $file->material->id . '/' . $file->name);
+        dd($path);
     }
 }
