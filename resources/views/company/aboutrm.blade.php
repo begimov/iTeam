3b5d00e9d5d5ff0ea58eabed6964d8257da3c7ad
begimov <begimov@gmail.com>
2018-04-05 17:41:40 +0300

    aboutrm blade template created

---
+++ resources/views/company/aboutrm.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col mt-5">
+                <h1>О русском менеджменте</h1>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                //
+            </div>
+        </div>
+    </div>
+@endsection
