c322a07dfd56cb635b72ae3cc4d5d958b355aec5
begimov <begimov@gmail.com>
2018-04-05 17:44:38 +0300

    aboutrm route added to navigation partial

--- resources/views/company/aboutrm.blade.php
+++ resources/views/company/aboutrm.blade.php
@@ -1,9 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
+    <div class="container my-5">
         <div class="row">
-            <div class="col mt-5">
+            <div class="col mb-4">
                 <h1>О русском менеджменте</h1>
             </div>
         </div>
