f6d67acb20c0d8af81b077e42d253e6e2780c753
begimov <begimov@gmail.com>
2018-04-17 10:46:59 +0300

    webhooks related route group and WO test route added

--- routes/web.php
+++ routes/web.php
@@ -43,3 +43,8 @@ Route::group(['prefix' => 'company', 'namespace' => 'Company'], function () {
 });
 
 Auth::routes();
+
+// Webhooks
+Route::group(['prefix' => 'webhooks', 'namespace' => 'Webhooks'], function () {
+    Route::get('walletone', 'WalletOneWebhookController@handleWebhook');
+});
