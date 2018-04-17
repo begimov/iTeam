715b0b5819e65e99f2bcc5d94865b568f7d487b0
begimov <begimov@gmail.com>
2018-04-17 11:33:44 +0300

    dynamic method calling implemented in handleWebhook method of WalletOneWebhookController

--- app/Http/Controllers/Webhooks/WalletOneWebhookController.php
+++ app/Http/Controllers/Webhooks/WalletOneWebhookController.php
@@ -7,5 +7,23 @@ use App\Http\Controllers\Controller;
 
 class WalletOneWebhookController extends Controller
 {
-    //
+    public function handleWebhook(Request $request)
+    {
+        $payload = $request->all();
+
+        // TODO: check if all necessary params exit
+
+        $method = 'handle'.$payload['WMI_ORDER_STATE'];
+
+        if (method_exists($this, $method)) {
+            return $this->{$method}($payload);
+        } else {
+            return $this->missingMethod();
+        }
+    }
+
+    protected function handleAccepted($payload)
+    {
+        return 1;
+    }
 }
