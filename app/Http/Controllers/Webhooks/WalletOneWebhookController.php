d5f4431d0ceed36b8ce09d374965dca8d8930d23
begimov <begimov@gmail.com>
2018-04-17 11:02:34 +0300

    WalletOneWebhookController created

---
+++ app/Http/Controllers/Webhooks/WalletOneWebhookController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Webhooks;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class WalletOneWebhookController extends Controller
+{
+    //
+}
