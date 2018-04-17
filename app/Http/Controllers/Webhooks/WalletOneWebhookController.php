f7795318f49cf2a484f3065e32c737a184279523
begimov <begimov@gmail.com>
2018-04-17 12:32:39 +0300

    payment handling logic moved to IWalletOne implementation

--- app/Http/Controllers/Webhooks/WalletOneWebhookController.php
+++ app/Http/Controllers/Webhooks/WalletOneWebhookController.php
@@ -20,54 +20,6 @@ class WalletOneWebhookController extends Controller
     {
         $payload = $request->all();
 
-        if(!$this->parametersExist($payload)) {
-            $this->printResponse("RETRY", "Required parameter is missing");
-        }
-
-        $method = 'handle'.$payload['WMI_ORDER_STATE'];
-
-        if (method_exists($this, $method)) {
-            return $this->{$method}($payload);
-        } else {
-            return $this->printResponse('RETRY', 'Method does not exist');
-        }
-    }
-
-    protected function handleAccepted($payload)
-    {
-        //
-    }
-
-    protected function parametersExist($payload)
-    {
-        return isset(
-            $payload['WMI_SIGNATURE'], 
-            $payload['WMI_PAYMENT_NO'], 
-            $payload['WMI_ORDER_STATE']
-        );
-    }
-
-    protected function isSignatureCorrect($payload)
-    {
-        $params = array_filter($payload, function ($key) {
-            return $key !== "WMI_SIGNATURE";
-        }, ARRAY_FILTER_USE_KEY);
-
-        uksort($params, "strcasecmp");
-
-        $values = array_reduce($params, function ($result, $elem) {
-            return $result . $elem;
-        }, "");
-
-        $signature = base64_encode(pack("H*", md5($values . config('services.walletone.key'))));
-
-        return $signature == $payload['WMI_SIGNATURE'];
-    }
-
-    protected function printResponse($status, $desc = '')
-    {
-        print "WMI_RESULT=" . strtoupper($status) . "&";
-        print "WMI_DESCRIPTION=" .urlencode($desc);
-        exit();
+        $this->walletOne->handlePayment($payload);
     }
 }
--- app/Services/Payments/WalletOne.php
+++ app/Services/Payments/WalletOne.php
@@ -15,7 +15,17 @@ class WalletOne implements IWalletOne
 
     public function handlePayment(array $payload)
     {
-        //
+        if(!$this->parametersExist($payload)) {
+            $this->printResponse("RETRY", "Required parameter is missing");
+        }
+
+        $method = 'handle'.$payload['WMI_ORDER_STATE'];
+
+        if (method_exists($this, $method)) {
+            return $this->{$method}($payload);
+        } else {
+            return $this->printResponse('RETRY', 'Method does not exist');
+        }
     }
 
     public function generatePaymentSignature(array $formFields)
@@ -36,5 +46,43 @@ class WalletOne implements IWalletOne
         return array_filter($formFields, function ($key) {
             return $key !== "WMI_SIGNATURE";
         }, ARRAY_FILTER_USE_KEY);
+    }
+
+    protected function handleAccepted($payload)
+    {
+        //
+    }
+
+    protected function parametersExist($payload)
+    {
+        return isset(
+            $payload['WMI_SIGNATURE'], 
+            $payload['WMI_PAYMENT_NO'], 
+            $payload['WMI_ORDER_STATE']
+        );
+    }
+
+    protected function isSignatureCorrect($payload)
+    {
+        $params = array_filter($payload, function ($key) {
+            return $key !== "WMI_SIGNATURE";
+        }, ARRAY_FILTER_USE_KEY);
+
+        uksort($params, "strcasecmp");
+
+        $values = array_reduce($params, function ($result, $elem) {
+            return $result . $elem;
+        }, "");
+
+        $signature = base64_encode(pack("H*", md5($values . config('services.walletone.key'))));
+
+        return $signature == $payload['WMI_SIGNATURE'];
+    }
+
+    protected function printResponse($status, $desc = '')
+    {
+        print "WMI_RESULT=" . strtoupper($status) . "&";
+        print "WMI_DESCRIPTION=" .urlencode($desc);
+        exit();
     }
 }
