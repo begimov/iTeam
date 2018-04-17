ec2ac886f374b6133d79f5421a43dd9714ff0d5c
begimov <begimov@gmail.com>
2018-04-17 12:29:44 +0300

    IWalletOne implementation injected in WalletOneWebhookController

--- app/Http/Controllers/Webhooks/WalletOneWebhookController.php
+++ app/Http/Controllers/Webhooks/WalletOneWebhookController.php
@@ -5,8 +5,17 @@ namespace App\Http\Controllers\Webhooks;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
+use App\Services\Payments\Contracts\IWalletOne;
+
 class WalletOneWebhookController extends Controller
 {
+    protected $walletOne;
+
+    public function __construct(IWalletOne $walletOne)
+    {
+        $this->walletOne = $walletOne;
+    }
+
     public function handleWebhook(Request $request)
     {
         $payload = $request->all();
