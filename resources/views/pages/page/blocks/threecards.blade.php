76aab62bce9ef1f17a251260d4cd25f18702f5fd
begimov <begimov@gmail.com>
2018-04-23 11:13:49 +0300

    template for a new block created

---
+++ resources/views/pages/page/blocks/threecards.blade.php
@@ -0,0 +1,32 @@
+<div class="row">
+  <div class="col-md-4">
+    <div class="card">
+        <div class="card-header">
+            {!! $element->data['title1'] !!}
+        </div>
+        <div class="card-body">
+            {!! $element->data['text1'] !!}
+        </div>
+    </div>
+  </div>
+  <div class="col-md-4">
+    <div class="card">
+        <div class="card-header">
+            {!! $element->data['title2'] !!}
+        </div>
+        <div class="card-body">
+            {!! $element->data['text2'] !!}
+        </div>
+    </div>
+  </div>
+  <div class="col-md-4">
+    <div class="card">
+        <div class="card-header">
+            {!! $element->data['title3'] !!}
+        </div>
+        <div class="card-body">
+            {!! $element->data['text3'] !!}
+        </div>
+    </div>
+  </div>
+</div>
