f14543807e6da18ccd138e7c47764be1318cfe06
begimov <begimov@gmail.com>
2018-04-25 17:16:09 +0300

    SubscribeOnMagnet form request pulled in MagnetController

--- app/Http/Controllers/Webapi/Pages/MagnetController.php
+++ app/Http/Controllers/Webapi/Pages/MagnetController.php
@@ -5,9 +5,11 @@ namespace App\Http\Controllers\Webapi\Pages;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
+use App\Http\Requests\Pages\SubscribeOnMagnet;
+
 class MagnetController extends Controller
 {
-    public function subscribe(Request $request)
+    public function subscribe(SubscribeOnMagnet $request)
     {
         return response()->json($request->all());
     }
--- app/Http/Requests/Pages/SubscribeOnMagnet.php
+++ app/Http/Requests/Pages/SubscribeOnMagnet.php
@@ -13,7 +13,7 @@ class SubscribeOnMagnet extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
