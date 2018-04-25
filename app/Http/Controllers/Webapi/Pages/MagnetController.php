0003714979023e52a8459eaccc09541136b70766
begimov <begimov@gmail.com>
2018-04-25 16:55:02 +0300

    MagnetController's template has been created

---
+++ app/Http/Controllers/Webapi/Pages/MagnetController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Webapi\Pages;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class MagnetController extends Controller
+{
+    public function register(Request $request)
+    {
+        //
+    }
+}
