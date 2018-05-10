6ea1e1a95e8216f97917ddd76a4601f05f95b2cc
begimov <begimov@gmail.com>
2018-05-10 12:02:01 +0300

    displaying of validation errors in profile settings

--- resources/views/users/profile/settings.blade.php
+++ resources/views/users/profile/settings.blade.php
@@ -7,31 +7,37 @@
                 <h1>Профиль</h1>
             </div>
         </div>
-        <form method="POST" action="{{ route('user.profile.settings.store') }}">
-            {{ csrf_field() }}
-            <div class="row">
-                <div class="col-sm-6">
+        
+        <div class="row">
+            <div class="col-sm-6">
+                <form method="POST" action="{{ route('user.profile.settings.store') }}">
+                    {{ csrf_field() }}
                     <div class="form-group">
                         <label for="email">Email</label>
-                        <input type="email" class="form-control" name="email" id="email" value="{{ old('email', auth()->user()->email) }}">
+                        <input type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" id="email" value="{{ old('email', auth()->user()->email) }}">
+                        <div class="invalid-feedback">
+                            {{ $errors->first('email') }}
+                        </div>
                     </div>
                     <div class="form-group">
                         <label for="name">Имя</label>
-                        <input type="text" class="form-control" name="name" id="name" value="{{ old('name', auth()->user()->name) }}">
+                        <input type="text" class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}" name="name" id="name" value="{{ old('name', auth()->user()->name) }}">
+                        <div class="invalid-feedback">
+                            {{ $errors->first('name') }}
+                        </div>
                     </div>
-                </div>
-                <div class="col-sm-6">
-                    {{-- <div class="form-group">
-                        <label for="company">Компания</label>
-                        <input type="text" class="form-control" name="company" id="company">
-                    </div> --}}
-                </div>
+                    <div class="form-group">
+                        <button type="submit" class="btn btn-primary">Сохранить</button>
+                    </div>
+                </form>
             </div>
-            <div class="row">
-                <div class="col">
-                    <button type="submit" class="btn btn-primary">Сохранить</button>
-                </div>
+            
+            <div class="col-sm-6">
+                {{-- <div class="form-group">
+                    <label for="company">Компания</label>
+                    <input type="text" class="form-control" name="company" id="company">
+                </div> --}}
             </div>
-        </form>
+        </div>
     </div>
 @endsection
