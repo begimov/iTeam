0e3d031a3d3dda55aed41d78c05fe69144846d3a
begimov <begimov@gmail.com>
2018-06-01 13:15:04 +0300

    Invoice's api method to get new invoice has been implemented

--- public/js/app.js
+++ public/js/app.js
@@ -50221,14 +50221,13 @@ exports.default = {
     });
   },
   getInvoice: function getInvoice(payload) {
-    console.log(payload);
-    // return new Promise((resolve, reject) => {
-    //   axios.get(`/webapi/orders/payments/invoices/create`).then(res => {
-    //     resolve(res)
-    //   }).catch(err => {
-    //     console.log(err)
-    //   })
-    // })
+    return new Promise(function (resolve, reject) {
+      axios.post("/webapi/orders/payments/invoices", payload).then(function (res) {
+        resolve(res);
+      }).catch(function (err) {
+        reject(err);
+      });
+    });
   }
 };
 
--- resources/assets/js/store/api/users/payments/invoice.js
+++ resources/assets/js/store/api/users/payments/invoice.js
@@ -9,13 +9,12 @@ export default {
     })
   },
   getInvoice(payload) {
-    console.log(payload)
-    // return new Promise((resolve, reject) => {
-    //   axios.get(`/webapi/orders/payments/invoices/create`).then(res => {
-    //     resolve(res)
-    //   }).catch(err => {
-    //     console.log(err)
-    //   })
-    // })
+    return new Promise((resolve, reject) => {
+      axios.post(`/webapi/orders/payments/invoices`, payload).then(res => {
+        resolve(res)
+      }).catch(err => {
+        reject(err)
+      })
+    })
   }
 }
