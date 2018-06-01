e2643eb83e4f8d7b19f3f978a9acfd96163e8445
begimov <begimov@gmail.com>
2018-06-01 12:05:26 +0300

    BusinessEntityRepository injected in InvoiceController

--- app/Http/Controllers/Webapi/Payments/InvoiceController.php
+++ app/Http/Controllers/Webapi/Payments/InvoiceController.php
@@ -5,8 +5,17 @@ namespace App\Http\Controllers\Webapi\Payments;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
+use App\Repositories\Contracts\Users\BusinessEntityRepository;
+
 class InvoiceController extends Controller
 {
+    protected $businessEntities;
+
+    public function __construct(BusinessEntityRepository $businessEntities)
+    {
+        $this->businessEntities = $businessEntities;
+    }
+
     /**
      * Show the form for creating a new resource.
      *
@@ -14,6 +23,6 @@ class InvoiceController extends Controller
      */
     public function create()
     {
-        return 1;
+        //
     }
 }
