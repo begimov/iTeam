3d79833e299790baad2dcc459ae632eae4c85846
begimov <begimov@gmail.com>
2018-06-01 11:47:49 +0300

    template InvoiceController has been created

---
+++ app/Http/Controllers/Webapi/Payments/InvoiceController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Webapi\Payments;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class InvoiceController extends Controller
+{
+    //
+}
