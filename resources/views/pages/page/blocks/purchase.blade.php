2dbb8c2425570ae17d32e4a9f77c35da1b309b51
begimov <begimov@gmail.com>
2018-06-15 11:24:47 +0300

    UI for selecting quantity of product added to purchase block template

--- resources/views/pages/page/blocks/purchase.blade.php
+++ resources/views/pages/page/blocks/purchase.blade.php
@@ -6,6 +6,17 @@
       {{ csrf_field() }}
       <input type="hidden" name="product_id" value="{{ $element->data['product']['productId'] }}">
       <input type="hidden" name="price_tag_id" value="{{ $element->data['product']['pricetagId'] }}">
+      @if($element->data['product']['isBundle'])
+        <div class="row justify-content-center">
+          <div class="col-sm-2">
+            <p>X</p>
+            <div class="form-group">
+              <label class="font-weight-bold">Кол-во:</label>
+              <input type="text" name="quantity" value="1" class="form-control">
+            </div>
+          </div>
+        </div>
+      @endif
       <button type="submit" class="btn btn-primary btn-lg">КУПИТЬ</button>
     </form>
     </p>
