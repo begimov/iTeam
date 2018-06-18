aedc3399b8ce5887a6204badc06bc21b707c8384
begimov <begimov@gmail.com>
2018-06-18 16:40:39 +0300

    imgparagraph block's template has been updated

--- resources/views/pages/page/blocks/imgparagraph.blade.php
+++ resources/views/pages/page/blocks/imgparagraph.blade.php
@@ -2,7 +2,7 @@
   <div class="col-md-4">
     <img src="{{ getElementsFilePath($element, 'img1') }}" class="img-fluid">
   </div>
-  <div class="col-md-8">
+  <div class="col-md-8 align-self-center lead">
     {!! $element->data['text'] !!}
   </div>
 </div>
