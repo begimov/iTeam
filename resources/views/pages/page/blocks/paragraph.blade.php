64975fe63a7ce31466a2625d3a1d93f5e3274d74
begimov <begimov@gmail.com>
2018-06-25 11:45:05 +0300

    displaying content on card has bee implemented in paragraph block's template

--- resources/views/pages/page/blocks/paragraph.blade.php
+++ resources/views/pages/page/blocks/paragraph.blade.php
@@ -1,5 +1,17 @@
+@if(isset($element->data['isCard']) && $element->data['isCard'])
+<div class="row my-4">
+  <div class="col-md-12 lead">
+    <div class="card card-default">
+      <div class="card-body">
+      {!! $element->data['text'] !!}
+      </div>
+    </div>
+  </div>
+</div>
+@else
 <div class="row my-4">
   <div class="col-md-12 lead">
     {!! $element->data['text'] !!}
   </div>
 </div>
+@endif
