71560a857a7f3cb83ca52de9f565e5ca6d380f83
begimov <begimov@gmail.com>
2018-06-25 12:01:32 +0300

    displaying content on card has bee implemented in imgparagraph block's template

--- resources/views/pages/page/blocks/imgparagraph.blade.php
+++ resources/views/pages/page/blocks/imgparagraph.blade.php
@@ -2,7 +2,18 @@
   <div class="col-md-4">
     <img src="{{ getElementsFilePath($element, 'img1') }}" class="img-fluid">
   </div>
+
+  @if(isset($element->data['isCard']) && $element->data['isCard'])
+  <div class="col-md-8 align-self-center lead">
+    <div class="card card-default">
+      <div class="card-body">
+        {!! $element->data['text'] !!}
+      </div>
+    </div>
+  </div>
+  @else
   <div class="col-md-8 align-self-center lead">
     {!! $element->data['text'] !!}
   </div>
+  @endif
 </div>
