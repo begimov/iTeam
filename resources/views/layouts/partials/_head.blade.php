9c21a19d0b4d9cafe2441d3c1d2ba056dba5270d
begimov <begimov@gmail.com>
2018-06-25 14:43:47 +0300

    alternative fonts and styles for product pages has been added

--- resources/views/layouts/partials/_head.blade.php
+++ resources/views/layouts/partials/_head.blade.php
@@ -4,6 +4,9 @@
 <!-- CSRF Token -->
 <meta name="csrf-token" content="{{ csrf_token() }}">
 <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,400i,900,900i&amp;subset=cyrillic" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Lora:700&amp;subset=cyrillic" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=PT+Sans:400,700&amp;subset=cyrillic" rel="stylesheet">
+
 <title>{{ config('app.name', 'Laravel') }}</title>
 <!-- Styles -->
 <link href="{{ asset('css/app.css') }}" rel="stylesheet">
