16ff08ad26312fac1b1a14bcd44c38e47c6825b8
begimov <begimov@gmail.com>
2018-06-27 13:54:21 +0300

    purchase block's template has been updated

--- resources/views/pages/page/blocks/purchase.blade.php
+++ resources/views/pages/page/blocks/purchase.blade.php
@@ -1,30 +1,33 @@
 <div class="container">
-  <div class="row my-5">
-    <div class="col text-center">
-      <h2>{{ getProductPrice($element->data['product']) }}~</h2>
-      <p>
-      <form action="{{ route('orders.store') }}" method="POST">
-        {{ csrf_field() }}
-        <input type="hidden" name="product_id" value="{{ $element->data['product']['productId'] }}">
-        <input type="hidden" name="price_tag_id" value="{{ $element->data['product']['pricetagId'] }}">
-        @if($element->data['product']['isBundle'])
-          <div class="row">
-            <div class="col">
-              <p>X</p>
-              <label class="font-weight-bold">Количество:</label>
-            </div>
-          </div>
-          <div class="row justify-content-center">
-            <div class="col-sm-2">  
-              <div class="form-group">
-                <input type="number" min="1" step="1" name="quantity" value="1" class="form-control">
+  <div class="row my-5 justify-content-center">
+    <div class="col-lg-4 col-md-6 col-sm-8 text-center">
+      <div class="card purchase-card">
+        <div class="card-header bg-dark text-light"></div>
+        <div class="card-body">
+          <h2><span class="badge badge-success price-badge">{{ getProductPrice($element->data['product']) }}~</span></h2>
+          <form action="{{ route('orders.store') }}" method="POST">
+            {{ csrf_field() }}
+            <input type="hidden" name="product_id" value="{{ $element->data['product']['productId'] }}">
+            <input type="hidden" name="price_tag_id" value="{{ $element->data['product']['pricetagId'] }}">
+            @if($element->data['product']['isBundle'])
+              <div class="row">
+                <div class="col">
+                  <p>X</p>
+                  <label class="font-weight-bold">Количество:</label>
+                </div>
               </div>
-            </div>
-          </div>
-        @endif
-        <button type="submit" class="btn btn-primary btn-lg">ПОЛУЧИТЬ</button>
-      </form>
-      </p>
+              <div class="row justify-content-center">
+                <div class="col-8">  
+                  <div class="form-group">
+                    <input type="number" min="1" step="1" name="quantity" value="1" class="form-control">
+                  </div>
+                </div>
+              </div>
+            @endif
+            <button type="submit" class="btn btn-primary btn-lg">ПОЛУЧИТЬ</button>
+          </form>
+        </div>
+      </div>
     </div>
   </div>
 </div>
