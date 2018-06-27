a243da7a7fab15d75bf554fda4022677e0b2105e
begimov <begimov@gmail.com>
2018-06-27 12:31:19 +0300

    title bg image added, parent container updated

--- resources/views/pages/page/blocks/title.blade.php
+++ resources/views/pages/page/blocks/title.blade.php
@@ -1,5 +1,9 @@
-<div class="row mt-5">
-  <div class="col-md-12">
-    <h1>{!! $element->data['text'] !!}</h1>
+<div class="container-fluid title-container">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12 my-5 py-5">
+        <h1 class="text-uppercase">{!! $element->data['text'] !!}</h1>
+      </div>
+    </div>
   </div>
 </div>
