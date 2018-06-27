a5f97035872d41c3f9f19b2c9b4b86c6ee670a96
begimov <begimov@gmail.com>
2018-06-27 12:33:27 +0300

    subtitle block's template has been updated to wrap in container

--- resources/views/pages/page/blocks/title.blade.php
+++ resources/views/pages/page/blocks/title.blade.php
@@ -1,7 +1,7 @@
 <div class="container-fluid title-container">
   <div class="container">
     <div class="row">
-      <div class="col-md-12 my-5 py-5">
+      <div class="col my-5 py-5">
         <h1 class="text-uppercase">{!! $element->data['text'] !!}</h1>
       </div>
     </div>
