61675b7580369decccbe1ab4bbc55ebd6c6c23b8
begimov <begimov@gmail.com>
2018-06-27 13:18:42 +0300

    threecards template has been updated

--- resources/views/pages/page/blocks/threecards.blade.php
+++ resources/views/pages/page/blocks/threecards.blade.php
@@ -2,8 +2,8 @@
     <div class="row my-4">
         <div class="col-md-4">
             <div class="card h-100">
-                <div class="card-header">
-                    <h5 class="mb-0 text-primary">{!! $element->data['title1'] !!}</h5>
+                <div class="card-header bg-info">
+                    <h5 class="mb-0 text-white">{!! $element->data['title1'] !!}</h5>
                 </div>
                 <div class="card-body">
                     {!! $element->data['text1'] !!}
@@ -12,8 +12,8 @@
         </div>
         <div class="col-md-4 mt-3 mt-sm-3 mt-md-0">
             <div class="card h-100">
-                <div class="card-header">
-                    <h5 class="mb-0 text-primary">{!! $element->data['title2'] !!}</h5>
+                <div class="card-header bg-info">
+                    <h5 class="mb-0 text-white">{!! $element->data['title2'] !!}</h5>
                 </div>
                 <div class="card-body">
                     {!! $element->data['text2'] !!}
@@ -22,8 +22,8 @@
         </div>
         <div class="col-md-4 mt-3 mt-sm-3 mt-md-0">
             <div class="card h-100">
-                <div class="card-header">
-                    <h5 class="mb-0 text-primary">{!! $element->data['title3'] !!}</h5>
+                <div class="card-header bg-info">
+                    <h5 class="mb-0 text-white">{!! $element->data['title3'] !!}</h5>
                 </div>
                 <div class="card-body">
                     {!! $element->data['text3'] !!}
