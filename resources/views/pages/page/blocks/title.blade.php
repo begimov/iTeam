1af379430b9a735d4ab3b52d6fe7450a2fe6548a
begimov <begimov@gmail.com>
2018-06-27 13:09:06 +0300

    title block's template has been updated to display subtitle part

--- resources/views/pages/page/blocks/title.blade.php
+++ resources/views/pages/page/blocks/title.blade.php
@@ -1,8 +1,9 @@
 <div class="container-fluid title-container">
   <div class="container">
     <div class="row">
-      <div class="col my-5 py-5">
+      <div class="col my-5 py-5 text-white text-center text-md-left">
         <h1 class="text-uppercase">{!! $element->data['text'] !!}</h1>
+        <h2 class="subtitle mt-3">{!! $element->data['subtext'] !!}</h1>
       </div>
     </div>
   </div>
