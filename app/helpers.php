87effb19af9da0196edd6e310d14bf99b28855f1
begimov <begimov@gmail.com>
2018-07-08 12:04:42 +0300

    check added to getElementsFilePath helper, reviewers avatars displaying implemented in textreviews template

--- app/helpers.php
+++ app/helpers.php
@@ -2,11 +2,14 @@
 if (!function_exists('getElementsFilePath')) {
 	function getElementsFilePath($element, $fileTag)
 	{
-		$file = $element->files->where('id', $element->data['files'][$fileTag])->first();
-	
-		return $file ? config('urls.files.page_elements_files') 
-			. '/' . $file->created_at->toDateString() 
-			. '/' . $file->name : '';
+		if (isset($element->data['files'][$fileTag])) {
+			$file = $element->files->where('id', $element->data['files'][$fileTag])->first();
+		
+			return $file ? config('urls.files.page_elements_files') 
+				. '/' . $file->created_at->toDateString() 
+				. '/' . $file->name : '';
+		}
+		return '';
 	}
 }
 
--- resources/views/pages/page/blocks/textreviews.blade.php
+++ resources/views/pages/page/blocks/textreviews.blade.php
@@ -4,14 +4,17 @@
         <h2>Отзывы наших участников:</h2>
     </div>
   </div>
-  <div class="row mt-4">
-    <div class="col">
-      @foreach ($element->data['reviews'] as $key => $review)
+  @foreach ($element->data['reviews'] as $key => $review)
+    <div class="row mt-4">
+      <div class="col-md-1 col-sm-2">
+        <img src="{{ getElementsFilePath($element, 'avatar') }}" class="img-fluid rounded">
+      </div>
+      <div class="col-md-11 col-sm-10">
         <blockquote class="blockquote">
           <p class="mb-0">{!! $review['text'] !!}</p>
           <footer class="blockquote-footer mb-4">{{$review['name']}}</footer>
         </blockquote>
-      @endforeach
+      </div>
     </div>
-  </div>
+  @endforeach
 </div>
