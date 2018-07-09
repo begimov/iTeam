bb71c1494a002c3fee8992d3f5b0497d9104d402
begimov <begimov@gmail.com>
2018-07-09 11:42:48 +0300

    ProductPolicy has been created and registered

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -15,6 +15,7 @@ class AuthServiceProvider extends ServiceProvider
     protected $policies = [
         'App\Models\Content\File' => 'App\Policies\Content\FilePolicy',
         'App\Models\Products\Order' => 'App\Policies\Products\OrderPolicy',
+        'App\Models\Products\Product' => 'App\Policies\Products\ProductPolicy',
     ];
 
     /**
