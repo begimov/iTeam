cf2b6986a9ae6d1e6c2ab8f6771b2c73ee233d5a
begimov <begimov@gmail.com>
2018-07-24 13:21:16 +0300

    bottom support signature added to page container template

--- resources/views/pages/page/container.blade.php
+++ resources/views/pages/page/container.blade.php
@@ -6,4 +6,13 @@
     @include('pages.page.blocks.' . $element->block->view)
   @endforeach
 </div>
+<div class="container mb-5 page-container">
+  <div class="row text-center">
+    <div class="col">
+      <h3>@lang('partials.footer.we_answer_all_questions')</h3>
+      <h4 class="mt-3"><a href="{{ config('urls.mail.info') }}">@lang('partials.header.email')</a></h4>
+      <h4 class="mt-3">@lang('partials.header.phone')</h4>
+    </div>
+  </div>
+</div>
 @endsection
