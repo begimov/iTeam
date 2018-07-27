cdbb6f289f1ddd8abf81844ff37d0fc4b6dc4c7a
begimov <begimov@gmail.com>
2018-07-27 15:17:15 +0300

    titles added to auth related templates

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,5 +1,9 @@
 @extends('layouts.app')
 
+@section('title')
+    <title>@lang('auth.register')</title>
+@endsection
+
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
