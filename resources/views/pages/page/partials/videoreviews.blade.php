c5d0fe8cbb144e4d50823955401004484a21e532
begimov <begimov@gmail.com>
2018-07-31 16:30:45 +0300

    video reviews added

--- resources/views/pages/page/partials/videoreviews.blade.php
+++ resources/views/pages/page/partials/videoreviews.blade.php
@@ -5,28 +5,40 @@
       </div>
   </div>
   <div class="row text-center">
-    <div class="col-md-6 mt-4">
+    <div class="col-lg-4 col-md-6 mt-4">
       <h5 class="mb-3">Александр Красный, «ГаРус»</h5>
       <youtube-player 
         video-id="Rp4Jh-DP7is"
       ></youtube-player>
     </div>
-    <div class="col-md-6 mt-4">
+    <div class="col-lg-4 col-md-6 mt-4">
       <h5 class="mb-3">Александр Соловьев, «FIS»</h5>
       <youtube-player 
         video-id="U4QFO0ILnmE"
       ></youtube-player>
     </div>
-    <div class="col-md-6 mt-4">
+    <div class="col-lg-4 col-md-6 mt-4">
       <h5 class="mb-3">Геннадий Черкасов, «Миг Плюс»</h5>
       <youtube-player 
         video-id="C2MVqTRj40w"
       ></youtube-player>
     </div>
-    <div class="col-md-6 mt-4">
+    <div class="col-lg-4 col-md-6 mt-4">
       <h5 class="mb-3">Яна Ходак, «МК Практик»</h5>
       <youtube-player 
         video-id="rlzpDK1my58"
+      ></youtube-player>
+    </div>
+    <div class="col-lg-4 col-md-6 mt-4">
+      <h5 class="mb-3">Артем Агабеков, «Фабрика Окон»</h5>
+      <youtube-player 
+        video-id="WWzZ19ZCCE8"
+      ></youtube-player>
+    </div>
+    <div class="col-lg-4 col-md-6 mt-4">
+      <h5 class="mb-3">Андрей Черкасов, «Миг Плюс»</h5>
+      <youtube-player 
+        video-id="MS8oCP5dIjc"
       ></youtube-player>
     </div>
   </div>
