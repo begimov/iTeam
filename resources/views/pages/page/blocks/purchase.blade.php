59771012dc516e1f157de4dac4a53dc03661ce3b
begimov <begimov@gmail.com>
2018-08-03 12:03:16 +0300

    responsive behaiviour of purchase block's template updated

--- resources/views/pages/page/blocks/purchase.blade.php
+++ resources/views/pages/page/blocks/purchase.blade.php
@@ -1,6 +1,6 @@
 <div class="container">
   <div class="row my-4 justify-content-center">
-    <div class="col-lg-4 col-md-6 col-sm-10">
+    <div class="col-lg-5 col-md-7 col-sm-10">
       @component('pages.page.blocks.components.purchase', ['element' => $element->data['product']])
       @endcomponent
     </div>
