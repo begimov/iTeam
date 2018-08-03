a543f00bb0dfb73397eb84094eaff309b533fadb
begimov <begimov@gmail.com>
2018-08-03 12:43:14 +0300

    images added for videoreviews page block

--- resources/views/pages/page/partials/videoreviews.blade.php
+++ resources/views/pages/page/partials/videoreviews.blade.php
@@ -16,30 +16,35 @@
       <h5 class="mb-3">Александр Соловьев, «FIS»</h5>
       <youtube-player 
         video-id="U4QFO0ILnmE"
+        bg-img="/img/pages/videoreviews/videoreview_02.jpg"
       ></youtube-player>
     </div>
     <div class="col-lg-4 col-md-6 mt-4">
       <h5 class="mb-3">Геннадий Черкасов, «Миг Плюс»</h5>
       <youtube-player 
         video-id="C2MVqTRj40w"
+        bg-img="/img/pages/videoreviews/videoreview_03.jpg"
       ></youtube-player>
     </div>
     <div class="col-lg-4 col-md-6 mt-4">
       <h5 class="mb-3">Яна Ходак, «МК Практик»</h5>
       <youtube-player 
         video-id="rlzpDK1my58"
+        bg-img="/img/pages/videoreviews/videoreview_04.jpg"
       ></youtube-player>
     </div>
     <div class="col-lg-4 col-md-6 mt-4">
       <h5 class="mb-3">Артем Агабеков, «Фабрика Окон»</h5>
       <youtube-player 
         video-id="WWzZ19ZCCE8"
+        bg-img="/img/pages/videoreviews/videoreview_05.jpg"
       ></youtube-player>
     </div>
     <div class="col-lg-4 col-md-6 mt-4">
       <h5 class="mb-3">Андрей Черкасов, «Миг Плюс»</h5>
       <youtube-player 
         video-id="MS8oCP5dIjc"
+        bg-img="/img/pages/videoreviews/videoreview_06.jpg"
       ></youtube-player>
     </div>
   </div>
