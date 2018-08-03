a510d42371d4371b35ce4eb31b386c43d7ba1399
begimov <begimov@gmail.com>
2018-08-03 15:07:51 +0300

    freemagnet block layout updated

--- resources/views/pages/page/blocks/freemagnet.blade.php
+++ resources/views/pages/page/blocks/freemagnet.blade.php
@@ -1,13 +1,17 @@
 <div class="container">
-  <div class="row my-4">
+  <div class="row my-5">
     <div class="col">
-      <free-magnet 
-        title="{{ $element->data['title'] }}"
-        description="{{ $element->data['description'] }}"
-        campaign-token="{{ $element->data['campaignToken'] }}"
-        button-text="{{ $element->data['buttonText'] }}"
-        redirect-url="{{ getElementsFilePath($element, 'doc1') }}"
-        layout-type="horizontal" />
+      <div class="card">
+        <div class="card-body py-4">
+          <free-magnet 
+            title="{{ $element->data['title'] }}"
+            description="{{ $element->data['description'] }}"
+            campaign-token="{{ $element->data['campaignToken'] }}"
+            button-text="{{ $element->data['buttonText'] }}"
+            redirect-url="{{ getElementsFilePath($element, 'doc1') }}"
+            layout-type="horizontal" />
+        </div>
+      </div>
     </div>
   </div>
 </div>
