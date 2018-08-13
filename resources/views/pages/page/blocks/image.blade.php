8807d58774cedb3692a03e724cc174b327bdb45b
begimov <begimov@gmail.com>
2018-08-13 16:03:34 +0300

    image block's template created

---
+++ resources/views/pages/page/blocks/image.blade.php
@@ -0,0 +1,7 @@
+<div class="container">
+  <div class="row my-4">
+    <div class="col text-center">
+      <img src="{{ getElementsFilePath($element, 'img1') }}" class="img-fluid rounded">
+    </div>
+  </div>
+</div>
