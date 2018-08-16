b8c6d9912f75515079f35095daf1136466bea377
begimov <begimov@gmail.com>
2018-08-16 13:04:51 +0300

    outputting latest mk page with links in index home template

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -191,78 +191,35 @@
     </div>
   </div>
   <div class="row">
-    <div class="col-xl-2 col-md-3 col-sm-6 p-2">
-      <a href="#" class="text-white">
-        <div class="card">
-          <div class="card-body text-center py-2">
-            <img src="https://iteam.ru/landing/template/img/mp_ychet.png" class="w-75">
-          </div>
-          <div class="card-footer bg-primary text-white">
-            Разрабатываем план маркетинга на 2018 год
-          </div>
+    @foreach($pages as $index => $page)
+      @if($index < 4)
+        <div class="col-xl-2 col-md-3 col-sm-6 p-2">
+          <a href="{{ route('pages.show', $page->slug) }}" class="text-white">
+            <div class="card">
+              <div class="card-body text-center py-2">
+                <img src="https://iteam.ru/landing/template/img/mp_ychet.png" class="w-75">
+              </div>
+              <div class="card-footer bg-primary text-white">
+                {{ $page->name }}
+              </div>
+            </div>
+          </a>
         </div>
-      </a>
-    </div>
-    <div class="col-xl-2 col-md-3 col-sm-6 p-2">
-      <a href="#" class="text-white">
-        <div class="card">
-          <div class="card-body text-center py-2">
-            <img src="https://iteam.ru/landing/template/img/mk30.png" class="w-75">
-          </div>
-          <div class="card-footer bg-primary text-white">
-            Разрабатываем план маркетинга на 2018 год
-          </div>
+      @else
+        <div class="col-xl-2 d-none d-xl-block p-2">
+          <a href="{{ route('pages.show', $page->slug) }}" class="text-white">
+            <div class="card">
+              <div class="card-body text-center py-2">
+                <img src="https://iteam.ru/landing/template/img/mk30.png" class="w-75">
+              </div>
+              <div class="card-footer bg-primary text-white">
+                {{ $page->name }}
+              </div>
+            </div>
+          </a>
         </div>
-      </a>
-    </div>
-    <div class="col-xl-2 col-md-3 col-sm-6 p-2">
-      <a href="#" class="text-white">
-        <div class="card">
-          <div class="card-body text-center py-2">
-            <img src="https://iteam.ru/landing/template/img/mp_nalogi.png" class="w-75">
-          </div>
-          <div class="card-footer bg-primary text-white">
-            Разрабатываем план маркетинга на 2018 год
-          </div>
-        </div>
-      </a>
-    </div>
-    <div class="col-xl-2 col-md-3 col-sm-6 p-2">
-      <a href="#" class="text-white">
-        <div class="card">
-          <div class="card-body text-center py-2">
-            <img src="https://iteam.ru/landing/template/img/mp_ychet.png" class="w-75">
-          </div>
-          <div class="card-footer bg-primary text-white">
-            Разрабатываем план маркетинга на 2018 год
-          </div>
-        </div>
-      </a>
-    </div>
-    <div class="col-xl-2 d-none d-xl-block p-2">
-      <a href="#" class="text-white">
-        <div class="card">
-          <div class="card-body text-center py-2">
-            <img src="https://iteam.ru/landing/template/img/mk30.png" class="w-75">
-          </div>
-          <div class="card-footer bg-primary text-white">
-            Разрабатываем план маркетинга на 2018 год
-          </div>
-        </div>
-      </a>
-    </div>
-    <div class="col-xl-2 d-none d-xl-block p-2">
-      <a href="#" class="text-white">
-        <div class="card">
-          <div class="card-body text-center py-2">
-            <img src="https://iteam.ru/landing/template/img/mp_nalogi.png" class="w-75">
-          </div>
-          <div class="card-footer bg-primary text-white">
-            Разрабатываем план маркетинга на 2018 год
-          </div>
-        </div>
-      </a>
-    </div>
+      @endif
+    @endforeach
   </div>
 </div>
 <!-- ARTICLES -->
