d48d78eda4cfbf7474aeb03b54cd1218311001d1
begimov <begimov@gmail.com>
2018-08-16 14:04:26 +0300

    latest mks displaying updated

--- resources/views/home/partials/_popularmks.blade.php
+++ resources/views/home/partials/_popularmks.blade.php
@@ -2,7 +2,7 @@
     @if($index < 4)
     <div class="col-xl-2 col-md-3 col-sm-6 p-2">
         <a href="{{ route('pages.show', $page->slug) }}" class="text-white">
-        <div class="card">
+        <div class="card h-100">
             <div class="card-body text-center py-2">
             <img src="{{ getElementsFilePath($page->elements[1], 'img1') }}" class="w-75">
             </div>
@@ -15,7 +15,7 @@
     @else
     <div class="col-xl-2 d-none d-xl-block p-2">
         <a href="{{ route('pages.show', $page->slug) }}" class="text-white">
-        <div class="card">
+        <div class="card h-100">
             <div class="card-body text-center py-2">
             <img src="{{ getElementsFilePath($page->elements[1], 'img1') }}" class="w-75">
             </div>
