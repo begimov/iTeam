8dd01cf1cd950da019bd5619ab9ee38a6cea14be
begimov <begimov@gmail.com>
2018-08-16 11:26:38 +0300

    video block's template created

---
+++ resources/views/pages/page/blocks/video.blade.php
@@ -0,0 +1,9 @@
+<div class="container">
+  <div class="row my-4 justify-content-center">
+    <div class="col-md-8 col-sm-10">
+      <div class="embed-responsive embed-responsive-16by9">
+        <iframe width="100%" height="350" src="{{ 'https://www.youtube.com/embed/' . $element->data['resources']['video1'] }}" frameborder="0" allow="encrypted-media" allowfullscreen></iframe>
+      </div>
+    </div>
+  </div>
+</div>
