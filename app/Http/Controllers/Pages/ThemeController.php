83940d97735e097dfda5eafc13ce04795adc7621
begimov <begimov@gmail.com>
2018-08-17 12:43:10 +0300

    ThemeController has been created

---
+++ app/Http/Controllers/Pages/ThemeController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Pages;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class ThemeController extends Controller
+{
+    public function index()
+    {
+        //
+    }
+}
