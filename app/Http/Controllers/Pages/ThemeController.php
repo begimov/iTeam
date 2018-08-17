ffe763d4ce3558433b96853a4bea7756285363f7
begimov <begimov@gmail.com>
2018-08-17 13:23:56 +0300

    retrieving pages within given category on specified theme

--- app/Http/Controllers/Pages/ThemeController.php
+++ app/Http/Controllers/Pages/ThemeController.php
@@ -10,10 +10,35 @@ use App\Models\Pages\{
     Theme
 };
 
+use App\Repositories\Eloquent\Criteria\Where;
+
+use App\Repositories\Contracts\Pages\{
+    PageRepository,
+    ThemeRepository
+};
+
 class ThemeController extends Controller
 {
+    protected $pages;
+
+    protected $themes;
+
+    public function __construct(PageRepository $pages, ThemeRepository $themes)
+    {
+        $this->pages = $pages;
+        
+        $this->themes = $themes;
+    }
+
     public function index(Category $category, Theme $theme)
     {
-        dd($category, $theme);
+        $pages = $this->pages->withCriteria([
+            new Where('category_id', $category->id),
+            new Where('theme_id', $theme->id)
+        ])->get();
+
+        $themes = $this->themes->get();
+
+        dd($pages, $themes);
     }
 }
