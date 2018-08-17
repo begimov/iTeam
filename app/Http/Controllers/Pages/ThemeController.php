c2973934c622fdabab67c8af5bf969419219588b
begimov <begimov@gmail.com>
2018-08-17 12:46:42 +0300

    binding category and theme in ThemeController's index method

--- app/Http/Controllers/Pages/ThemeController.php
+++ app/Http/Controllers/Pages/ThemeController.php
@@ -5,10 +5,15 @@ namespace App\Http\Controllers\Pages;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
+use App\Models\Pages\{
+    Category,
+    Theme
+};
+
 class ThemeController extends Controller
 {
-    public function index()
+    public function index(Category $category, Theme $theme)
     {
-        //
+        dd($category, $theme);
     }
 }
