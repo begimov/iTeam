12c0abbd710905b3d2275e33af95a62f13daf98b
begimov <begimov@gmail.com>
2018-08-21 16:19:23 +0300

    date field added to title block

--- resources/views/pages/page/blocks/title.blade.php
+++ resources/views/pages/page/blocks/title.blade.php
@@ -3,7 +3,14 @@
     <div class="row">
       <div class="col my-5 py-5 text-white text-center text-md-left">
         <h1 class="text-uppercase">{!! $element->data['text'] !!}</h1>
-        <h2 class="subtitle mt-3">{!! $element->data['subtext'] !!}</h1>
+        <h2 class="subtitle mt-3">{!! $element->data['subtext'] !!}</h2>
+        @isset($element->data['comment'])
+          <h2 class="subtitle mt-3">
+            <span class="badge badge-success">
+              {!! $element->data['comment'] !!}
+            </span>
+          </h2>
+        @endisset
       </div>
     </div>
   </div>
