d2d0db779ff5e4e6551c0f762bbd2388d3978db7
begimov <begimov@gmail.com>
2018-08-23 14:42:37 +0300

    services page's layout updated

--- resources/views/company/services.blade.php
+++ resources/views/company/services.blade.php
@@ -140,37 +140,64 @@
             </div>
         </div>
     </div>
-    <div class="container">
+    <!-- <div class="container">
         <div class="row mt-3 mb-2">
             <div class="col text-center">
                 <p><a href="#" class="btn btn-light btn-sm">Смотреть больше</a></p>
             </div>
         </div>
-    </div>
+    </div> -->
 
     <!-- REVIEWS -->
-    <div class="container-fluid py-5">
-        <div class="row">
-            <div class="col text-center">
-                <h2 class="font-weight-light mb-3">Отзывы</h2>
+    <div class="container mb-5">
+        <div class="row text-center mt-5">
+            <div class="col">
+                <h2 class="font-weight-light">Отзывы наших клиентов:</h2>
             </div>
         </div>
-        <div class="row">
-            <div class="col-lg-4 text-center services-video-1 p-5">
-                <youtube-player
+        <div class="row text-center">
+            <div class="col-lg-4 col-md-6 mt-4">
+                <h5 class="mb-3">Александр Красный, «ГаРус»</h5>
+                <youtube-player 
                     video-id="Rp4Jh-DP7is"
+                    bg-img="/img/pages/videoreviews/videoreview_01.jpg"
                 ></youtube-player>
             </div>
-            <div class="col-lg-4 text-center services-video-2 p-5">
-                <youtube-player
+            <div class="col-lg-4 col-md-6 mt-4">
+                <h5 class="mb-3">Александр Соловьев, «FIS»</h5>
+                <youtube-player 
                     video-id="U4QFO0ILnmE"
+                    bg-img="/img/pages/videoreviews/videoreview_02.jpg"
                 ></youtube-player>
             </div>
-            <div class="col-lg-4 text-center services-video-3 p-5">
-                <youtube-player
+            <div class="col-lg-4 col-md-6 mt-4">
+                <h5 class="mb-3">Геннадий Черкасов, «Миг Плюс»</h5>
+                <youtube-player 
                     video-id="C2MVqTRj40w"
+                    bg-img="/img/pages/videoreviews/videoreview_03.jpg"
                 ></youtube-player>
             </div>
+            <div class="col-lg-4 col-md-6 mt-4">
+                <h5 class="mb-3">Яна Ходак, «МК Практик»</h5>
+                <youtube-player 
+                    video-id="rlzpDK1my58"
+                    bg-img="/img/pages/videoreviews/videoreview_04.jpg"
+                ></youtube-player>
+            </div>
+            <div class="col-lg-4 col-md-6 mt-4">
+                <h5 class="mb-3">Артем Агабеков, «Фабрика Окон»</h5>
+                <youtube-player 
+                    video-id="WWzZ19ZCCE8"
+                    bg-img="/img/pages/videoreviews/videoreview_05.jpg"
+                ></youtube-player>
+            </div>
+            <div class="col-lg-4 col-md-6 mt-4">
+                <h5 class="mb-3">Андрей Черкасов, «Миг Плюс»</h5>
+                <youtube-player 
+                    video-id="MS8oCP5dIjc"
+                    bg-img="/img/pages/videoreviews/videoreview_06.jpg"
+                ></youtube-player>
+                </div>
         </div>
     </div>
 @endsection
