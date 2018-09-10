b6f801cf14ec31d8e2d76a80ada8c2e68f37494d
begimov <begimov@gmail.com>
2018-09-10 11:50:06 +0300

    wo config updated

--- resources/assets/js/config/payments.js
+++ resources/assets/js/config/payments.js
@@ -1,5 +1,5 @@
 export default {
-    WMI_MERCHANT_ID: "132360589069",
+    WMI_MERCHANT_ID: "107649891291",
     WMI_CURRENCY_ID: "643",
     WMI_AUTO_LOCATION: "1",
     WMI_SUCCESS_URL: "https://iteam.ru/user",
