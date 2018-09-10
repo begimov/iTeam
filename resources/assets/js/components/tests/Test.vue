8fc7a6ba838198c6483a02bf10cce6e2e76e14aa
begimov <begimov@gmail.com>
2018-09-10 12:40:15 +0300

    basic Test component created

---
+++ resources/assets/js/components/tests/Test.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="container">
+        <!-- <div v-bind:class="{ 'isActive': isLoading, 'loader': true, 'loader-def': true }"></div> -->
+
+        <div class="row">
+            <div class="col">
+                <h1 class="mb-4">Тест</h1>
+            </div>
+        </div>
+            
+    </div>
+</template>
+
+<script src="./test.js"></script>
---
+++ resources/assets/js/components/tests/test.js
@@ -0,0 +1,17 @@
+import { mapActions, mapGetters } from 'vuex'
+
+export default {
+    computed: {
+        // ...mapGetters('users/dashboard', [
+        //     'isLoading',
+        // ])
+    },
+    methods: {
+        // ...mapActions('users/dashboard', [
+        //     'getOrders',
+        // ])
+    },
+    mounted() {
+        // this.getOrders();
+    }
+};
