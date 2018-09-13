356b8bcd205faa8c84c4738492e475d0a75fd604
begimov <begimov@gmail.com>
2018-09-13 12:26:40 +0300

    TestResult's relationship methods created

--- app/Models/Tests/TestResult.php
+++ app/Models/Tests/TestResult.php
@@ -2,9 +2,19 @@
 
 namespace App\Models\Tests;
 
+use App\User;
+use App\Models\Tests\Test;
 use Illuminate\Database\Eloquent\Model;
 
 class TestResult extends Model
 {
-    //
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function test()
+    {
+        return $this->belongsTo(Test::class);
+    }
 }
