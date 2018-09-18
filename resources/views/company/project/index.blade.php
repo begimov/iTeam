169824fba0ce519e21d6435f2feb907741054575
begimov <begimov@gmail.com>
2018-09-18 17:08:17 +0300

    basic popover with projects details implemented

--- resources/views/company/project/index.blade.php
+++ resources/views/company/project/index.blade.php
@@ -12,28 +12,39 @@
             </div>
         </div>
 
-        <div class="row">
+        <div class="row" id="projects-container">
+
             <div class="col-xl-4 col-lg-6 mt-3">
-                <div class="card">
+                <div class="card text-center">
                     <div class="card-body">
-                        //
+                        <h5 class="card-title">Компания &laquo;АСТ&raquo; г. Краснодар</h5>
+                        <p class="card-text">
+                            Дистрибуция сельскохозяйственной и&nbsp;дорожно-строительной техники
+                        </p>
+                        <button type="button" class="btn btn-primary" data-toggle="popover" data-placement="top" data-trigger="click" 
+                            data-content="<ul class='mb-1'>
+                                    <li>Разработка Положения об организационной структуре</li>
+                                    <li>Разработка показателей KPI</li>
+                                </ul>">Подробнее</button>
+                    </div>
+                    <div class="card-footer bg-white">
+                        <a href="https://act.su" target="_blank">https://act.su</a>
                     </div>
                 </div>
             </div>
-            <div class="col-xl-4 col-lg-6 mt-3">
-                <div class="card">
-                    <div class="card-body">
-                        //
-                    </div>
-                </div>
-            </div>
-            <div class="col-xl-4 col-lg-6 mt-3">
-                <div class="card">
-                    <div class="card-body">
-                        //
-                    </div>
-                </div>
-            </div>
+
         </div>
     </div>
+@endsection
+
+@section('postJquery')
+    $('[data-toggle="popover"]').popover({
+        html: true
+    })
+    $('[data-toggle="popover"]').on('shown.bs.popover', function () {
+        $(this).append( "<div id='projects-popover-bg' class='w-100 h-100' style='position:fixed;left:0;top:0;background-color:#00000099;'>&nbsp;</div>" )
+    })
+    $('[data-toggle="popover"]').on('hidden.bs.popover', function () {
+        $('#projects-popover-bg').remove()
+    })
 @endsection
