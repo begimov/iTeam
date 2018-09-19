dc8bac30ce1578fec20e420202c24c5ece682003
begimov <begimov@gmail.com>
2018-09-19 09:07:10 +0300

    jquery bootstrap related scripts moved to Projects component

--- resources/views/company/project/index.blade.php
+++ resources/views/company/project/index.blade.php
@@ -7,15 +7,3 @@
 @section('content')
     <company-projects />
 @endsection
-
-@section('postJquery')
-    $('[data-toggle="popover"]').popover({
-        html: true
-    })
-    $('[data-toggle="popover"]').on('shown.bs.popover', function () {
-        $(this).append( "<div id='projects-popover-bg' class='w-100 h-100' style='position:fixed;left:0;top:0;background-color:#00000099;'>&nbsp;</div>" )
-    })
-    $('[data-toggle="popover"]').on('hidden.bs.popover', function () {
-        $('#projects-popover-bg').remove()
-    })
-@endsection
