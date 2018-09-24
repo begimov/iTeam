e6c3fbfb3cb8a48c32f66a9512ebbad2f7922cdc
begimov <begimov@gmail.com>
2018-09-24 18:41:17 +0300

    EloquentReviewRepository created and binded to its interface

--- resources/views/company/review/index.blade.php
+++ resources/views/company/review/index.blade.php
@@ -1,1 +1,20 @@
-REVIEWS
+@extends('layouts.app')
+
+@section('title')
+    <title>Отзывы</title>
+@endsection
+
+@section('content')
+<div class="container my-5">
+        <div class="row">
+            <div class="col">
+                <h1>Отзывы</h1>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-3">
+                //
+            </div>
+        </div>
+    </div>
+@endsection
