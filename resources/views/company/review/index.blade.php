4c64768ef2842cad180d90794a40ff466681f637
begimov <begimov@gmail.com>
2018-09-24 19:36:29 +0300

    basic reviews displaying layout updated

--- resources/views/company/review/index.blade.php
+++ resources/views/company/review/index.blade.php
@@ -5,39 +5,39 @@
 @endsection
 
 @section('content')
-<div class="container-fluid bg-dark">
-    <div class="row">
-        <div class="container my-5">
-            <div class="row">
-                <div class="col">
-                    <h1 class="mb-4 text-light">Отзывы</h1>
+    <div class="container-fluid bg-dark">
+        <div class="row">
+            <div class="container my-5">
+                <div class="row">
+                    <div class="col">
+                        <h1 class="text-light">Отзывы</h1>
+                    </div>
                 </div>
-            </div>
-            @foreach ($reviews as $review)
-            <div class="row">
-                <div class="col">
-                    <div class="speech-bubble p-3">
-                        <div class="row">
-                            <div class="col-2">
-                                //
-                            </div>
-                            <div class="col-10">
-                                <blockquote class="blockquote">
-                                    <p class="mb-0">&laquo;{{ $review->quote }}&raquo;</p>
-                                    <footer class="blockquote-footer mt-1 text-right">{{ $review->author }} / <cite>{{ $review->position }}</cite></footer>
-                                </blockquote>
+                @foreach ($reviews as $key => $review)
+                    <div class="row mt-5">
+                        <div class="col">
+                            <div class="speech-bubble-{{ random_int(1,4) }} p-4 shadow-lg">
+                                <div class="row">
+                                    <div class="col-2 {{ ($key % 2 == 0) ? '' : 'order-2' }}">
+                                        //
+                                    </div>
+                                    <div class="col-10">
+                                        <blockquote class="blockquote">
+                                            <p class="mb-0">&laquo;{{ $review->quote }}&raquo;</p>
+                                            <footer class="blockquote-footer mt-3 {{ ($key % 2 == 0) ? 'text-right' : '' }}">{{ $review->author }} / <cite>{{ $review->position }}</cite></footer>
+                                        </blockquote>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
-            </div>
-            @endforeach
-            <div class="row">
-                <div class="col">
-                    {{ $reviews->links("pagination::bootstrap-4") }}
+                @endforeach
+                <div class="row">
+                    <div class="col">
+                        {{ $reviews->links("pagination::bootstrap-4") }}
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
