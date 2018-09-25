dfe8812f5c46e656529fe6105691eeb7afdfbdd9
begimov <begimov@gmail.com>
2018-09-25 10:41:09 +0300

    ReviewRepository injected into CompanyController

--- app/Http/Controllers/Company/CompanyController.php
+++ app/Http/Controllers/Company/CompanyController.php
@@ -4,9 +4,18 @@ namespace App\Http\Controllers\Company;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Repositories\Eloquent\Criteria\With;
+use App\Repositories\Contracts\Reviews\ReviewRepository;
 
 class CompanyController extends Controller
 {
+    protected $reviews;
+
+    public function __construct(ReviewRepository $reviews)
+    {
+        $this->reviews = $reviews;
+    }
+
     public function services()
     {
         return view('company.services');
