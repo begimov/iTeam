218eb5a018c9142f9676bc0c6018d52fb00808ab
begimov <begimov@gmail.com>
2018-10-02 12:57:40 +0300

    review index page layout updated

--- resources/views/company/review/index.blade.php
+++ resources/views/company/review/index.blade.php
@@ -18,12 +18,14 @@
                         <div class="col">
                             <div class="speech-bubble-{{ random_int(1,4) }} p-4 shadow-lg">
                                 <div class="row">
-                                    <div class="col-lg-2 col-md-3 d-none d-sm-none d-md-block {{ ($key % 2 == 0) ? '' : 'order-2' }}">
-                                        <img src="{{ getElementsFilePath($review, '') }}" class="img-fluid rounded">
-                                    </div>
-                                    <div class="col-lg-10 col-md-9">
+                                    @if($avatarUrl = getElementsFilePath($review, ''))
+                                        <div class="col-lg-2 col-md-3 d-none d-sm-none d-md-block {{ ($key % 2 == 0) ? '' : 'order-2' }}">
+                                            <img src="{{ $avatarUrl }}" class="img-fluid rounded">
+                                        </div>
+                                    @endif
+                                    <div class="{{ $avatarUrl ? 'col-lg-10 col-md-9' : 'col' }}">
                                         <blockquote class="blockquote">
-                                            <p class="mb-0">&laquo;{{ $review->quote }}&raquo;</p>
+                                            <p class="mb-0">&laquo;{!! nl2br(e($review->quote)) !!}&raquo;</p>
                                             <footer class="blockquote-footer mt-3 {{ ($key % 2 == 0) ? 'text-right' : '' }}">{{ $review->author }} / <cite>{{ $review->position }}</cite></footer>
                                         </blockquote>
                                     </div>
