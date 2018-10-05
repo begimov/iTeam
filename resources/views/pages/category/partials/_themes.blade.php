6b10cd8bb01288ece6ca985403009dd5f06fcec3
begimov <begimov@gmail.com>
2018-10-05 09:52:24 +0300

    one theme temporary hidded

--- resources/views/pages/category/partials/_themes.blade.php
+++ resources/views/pages/category/partials/_themes.blade.php
@@ -6,11 +6,13 @@
             </span>
         </span>
         @foreach ($themes as $theme)
-            <span class="badge badge-secondary my-1 p-2">
-                <span class="lead">
-                    <a href="{{ route('theme.index', ['category' => $category->slug, 'theme' => $theme->slug]) }}">{{ $theme->name }}</a>
+            @if($theme->id !== 6)
+                <span class="badge badge-secondary my-1 p-2">
+                    <span class="lead">
+                        <a href="{{ route('theme.index', ['category' => $category->slug, 'theme' => $theme->slug]) }}">{{ $theme->name }}</a>
+                    </span>
                 </span>
-            </span>
+            @endif
         @endforeach
     </div>
 </div>
