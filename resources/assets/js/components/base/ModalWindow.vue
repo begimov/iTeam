8c6b2758f508fc93a1022d73a3e3858cd921c08d
begimov <begimov@gmail.com>
2018-10-16 09:36:15 +0300

    basic ModalWindow component

---
+++ resources/assets/js/components/base/ModalWindow.vue
@@ -0,0 +1,8 @@
+<template>
+    <free-magnet 
+            description="Книга &laquo;Как внедрить бизнес-процессы&raquo;!"
+            campaign-token="VjVkP"
+            redirect-url="{{ asset('files/magnets/process_approach_book.pdf') }}" />
+</template>
+
+<script src="./modalwindow.js"></script>
---
+++ resources/assets/js/components/base/modalwindow.js
@@ -0,0 +1,9 @@
+export default {
+    name: 'ModalWindow',
+    computed: {
+        //
+    },
+    mounted() {
+        //
+    }
+}
