30bb7b70639f59457e108466eb539d5b798b11d6
begimov <begimov@gmail.com>
2018-10-17 12:09:00 +0300

    basic Ya Kassa lib set up

--- app/Providers/PaymentServiceProvider.php
+++ app/Providers/PaymentServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use App\Services\Payments\Contracts\IWalletOne;
 use App\Services\Payments\WalletOne;
+use YandexCheckout\Client;
 
 class PaymentServiceProvider extends ServiceProvider
 {
@@ -15,7 +16,14 @@ class PaymentServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        $this->app->bind('YandexCheckout\Client', function ($app) {
+
+            $client = new Client();
+
+            $client->setAuth(env('YA_KASSA_ID'), env('YA_KASSA_KEY'));
+
+            return $client;
+        });
     }
 
     /**
