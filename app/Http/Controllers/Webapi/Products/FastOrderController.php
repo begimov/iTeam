dc7f9e23a4b5dbb4ddd4cf9af1cb9302401745f1
begimov <begimov@gmail.com>
2018-10-27 07:25:32 +0300

    config files artisan bug fixed, FastOrderController created

---
+++ app/Http/Controllers/Webapi/Products/FastOrderController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Webapi\Products;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class FastOrderController extends Controller
+{
+    //
+}
