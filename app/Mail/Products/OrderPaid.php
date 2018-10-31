d455069353fa88bf457757bae4c0d06c87e558eb
begimov <begimov@gmail.com>
2018-10-31 09:23:34 +0300

    OrderPaid notification class and mailable implemented

--- app/Mail/Products/OrderPaid.php
+++ app/Mail/Products/OrderPaid.php
@@ -7,7 +7,7 @@ use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Contracts\Queue\ShouldQueue;
 
-class OrderPayed extends Mailable
+class OrderPaid extends Mailable
 {
     use Queueable, SerializesModels;
 
---
+++ app/Notifications/Products/OrderPaid.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Notifications\Products;
+
+use Illuminate\Bus\Queueable;
+use App\Mail\Products\OrderPaid as OrderPaidMailable;
+use Illuminate\Notifications\Notification;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+
+class OrderPaid extends Notification
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new OrderPaidMailable())->to($notifiable->email);
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
