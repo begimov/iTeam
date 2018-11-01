085da55c08000e90b69931125673f152712c05c5
begimov <begimov@gmail.com>
2018-11-01 08:49:55 +0300

    additional page related tests implemented

--- tests/Feature/Pages/PageTest.php
+++ tests/Feature/Pages/PageTest.php
@@ -2,7 +2,9 @@
 
 namespace Tests\Feature\Pages;
 
+use App\User;
 use Tests\TestCase;
+use App\Models\Pages\Page;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
@@ -47,14 +49,45 @@ class PageTest extends TestCase
 
     public function tests_page_view()
     {
-        $response = $this->get('/pages/page');
+        $page = factory(Page::class)->create();
+        
+        $response = $this->get('/pages/' . $page->slug);
 
         $response->assertViewIs('pages.page.container');
     }
 
+    public function tests_last_visited_page_path_store_in_session_for_unauth_user()
+    {
+        $page = factory(Page::class)->create();
+        
+        $response = $this->get('/pages/' . $page->slug);
+
+        $response->assertSessionHas(config('session.keys.page_path'), request()->path());
+    }
+
+    public function tests_last_visited_page_redirect_for_auth_user()
+    {
+        $page = factory(Page::class)->create();
+        
+        $this->get($url = '/pages/' . $page->slug);
+
+        $user = factory(User::class)->create([
+            'password' => bcrypt($password = 'i-love-laravel'),
+        ]);
+
+        $response = $this->post('/login', [
+            'email' => $user->email,
+            'password' => $password,
+        ]);
+
+        $response->assertRedirect($url);
+    }
+
     public function tests_page_view_has_page_data()
     {
-        $response = $this->get('/pages/page');
+        $page = factory(Page::class)->create();
+        
+        $response = $this->get('/pages/' . $page->slug);
 
         $response->assertViewHas('page');
     }
@@ -64,5 +97,40 @@ class PageTest extends TestCase
         $response = $this->get('/pages/undefined');
 
         $response->assertStatus(404);
+    }
+
+    public function tests_unpublished_page_not_available_on_category_page()
+    {
+        $page = factory(Page::class)->create([
+            'status' => 0,
+            'category_id' => 1
+        ]);
+        
+        $response = $this->get('/pages/category/' . config('urls.slugs.categories.mk'));
+
+        $response->assertDontSeeText($page->name);
+    }
+
+    public function tests_published_page_is_available_on_category_page()
+    {
+        $page = factory(Page::class)->create([
+            'category_id' => 1
+        ]);
+        
+        $response = $this->get('/pages/category/' . config('urls.slugs.categories.mk'));
+
+        $response->assertSeeText($page->name);
+    }
+
+    public function tests_unpublished_page_is_available_via_direct_link()
+    {
+        $page = factory(Page::class)->create([
+            'status' => 0,
+            'category_id' => 1
+        ]);
+        
+        $response = $this->get('/pages/' . $page->slug);
+
+        $response->assertSuccessful();
     }
 }
