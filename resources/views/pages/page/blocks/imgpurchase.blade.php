ca3b1f860a58c35297ed2693767fa65755e75498
begimov <begimov@gmail.com>
2018-11-09 09:42:26 +0300

    imgpurchase styling updated

--- resources/views/pages/page/blocks/imgpurchase.blade.php
+++ resources/views/pages/page/blocks/imgpurchase.blade.php
@@ -1,11 +1,11 @@
 <div class="container">
   <div class="row my-4">
-    <div class="{{ 'col-lg-6 text-center text-md-left align-self-center'
+    <div class="{{ 'col-lg-4 text-center text-md-left align-self-center'
       .((isset($element->data['reversed']) && $element->data['reversed']) ? ' order-md-last' : '') }}">
       <img src="{{ getElementsFilePath($element, 'img1') }}" class="img-fluid rounded">
     </div>
 
-    <div class="col-lg-6 my-4 align-self-center">
+    <div class="col-lg-8 my-4 align-self-center">
       @component('pages.page.blocks.components.purchase', [
         'element' => $element->data['product'], 
         'isFastOrder' => $element->data['product']['isFastOrder'] ?? NULL
