3401614cffd917af73bce3ea6fda234dd952460e
begimov <begimov@gmail.com>
2018-11-10 13:27:51 +0300

    404 templated layout updated

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -6,7 +6,7 @@
 
 @section('content')
 <div class="container">
-  <div class="row">
+  <div class="row text-center">
     <div class="col mt-5">
       <div class="card text-white bg-danger">
         <div class="card-body"><h4 class="mb-0">{{ trans('validation.exception_errors.404') }}</h4></div>
@@ -16,7 +16,9 @@
   <div class="row">
     <div class="col mt-3">
       <div class="card">
-        <div class="card-body">
+        <div class="card-body text-center">
+          <img src="{{ asset('img/home/gift_box.png') }}" class="img-fluid">
+          <h2>Приносим извинения и дарим</h2>
           <free-magnet 
             description="Книга &laquo;Как внедрить бизнес-процессы&raquo;!"
             campaign-token="VjVkP"
@@ -25,6 +27,11 @@
             redirect-url="{{ config('urls.magnets.processes_book') }}" />
         </div>
       </div>
+    </div>
+  </div>
+  <div class="row text-center mt-3 mb-5">
+    <div class="col">
+      <a href="{{ route('home') }}">Вернуться на главную</a>
     </div>
   </div>
 </div>
