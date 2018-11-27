40a491573fe74d2ae5c8caa0e0571fc55260299b
begimov <begimov@gmail.com>
2018-11-27 09:53:01 +0300

    title displaying in freeproduct block

--- resources/views/pages/page/blocks/freeproduct.blade.php
+++ resources/views/pages/page/blocks/freeproduct.blade.php
@@ -4,7 +4,7 @@
       <div class="card">
         <div class="card-body py-4">
           <free-magnet 
-            title="ПОЛУЧИТЬ БЕСПЛАТНО"
+            title="{{ isset($element->data['form']['title']) && $element->data['form']['title'] ? $element->data['form']['title'] : 'ПОЛУЧИТЬ БЕСПЛАТНО' }}"
             campaign-token="{{ $element->data['form']['campaignToken'] }}"
             redirect-url="{{ '/products/' . $element->data['form']['productId'] }}"
             layout-type="horizontal"
