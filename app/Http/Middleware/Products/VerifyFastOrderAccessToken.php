ffd252c6b125746ff8b8344abe18073c7bce5ad2
begimov <begimov@gmail.com>
2018-11-27 09:37:11 +0300

    only anonymous fastorders available via direct link

--- app/Http/Middleware/Products/VerifyFastOrderAccessToken.php
+++ app/Http/Middleware/Products/VerifyFastOrderAccessToken.php
@@ -15,7 +15,8 @@ class VerifyFastOrderAccessToken
      */
     public function handle($request, Closure $next)
     {
-        if ($request->route('code') !== base64_encode($request->route('order')->created_at)) {
+        if ($request->route('code') !== base64_encode($request->route('order')->created_at) 
+            || !is_null($request->route('order')->user_id)) {
 
             return redirect()->route('home');
             
