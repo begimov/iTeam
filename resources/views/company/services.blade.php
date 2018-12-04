cf030f8f3539e741d9c87ac0a92af9284ea40148
begimov <begimov@gmail.com>
2018-12-04 16:17:40 +0300

    videoreviews updated

--- resources/views/company/services.blade.php
+++ resources/views/company/services.blade.php
@@ -155,50 +155,9 @@
                 <h2 class="font-weight-light">Отзывы наших клиентов:</h2>
             </div>
         </div>
-        <div class="row text-center">
-            <div class="col-lg-4 col-md-6 mt-4">
-                <h5 class="mb-3">Александр Красный, «ГаРус»</h5>
-                <youtube-player 
-                    video-id="Rp4Jh-DP7is"
-                    bg-img="/img/pages/videoreviews/videoreview_01.jpg"
-                ></youtube-player>
-            </div>
-            <div class="col-lg-4 col-md-6 mt-4">
-                <h5 class="mb-3">Александр Соловьев, «FIS»</h5>
-                <youtube-player 
-                    video-id="U4QFO0ILnmE"
-                    bg-img="/img/pages/videoreviews/videoreview_02.jpg"
-                ></youtube-player>
-            </div>
-            <div class="col-lg-4 col-md-6 mt-4">
-                <h5 class="mb-3">Геннадий Черкасов, «Миг Плюс»</h5>
-                <youtube-player 
-                    video-id="C2MVqTRj40w"
-                    bg-img="/img/pages/videoreviews/videoreview_03.jpg"
-                ></youtube-player>
-            </div>
-            <div class="col-lg-4 col-md-6 mt-4">
-                <h5 class="mb-3">Яна Ходак, «МК Практик»</h5>
-                <youtube-player 
-                    video-id="rlzpDK1my58"
-                    bg-img="/img/pages/videoreviews/videoreview_04.jpg"
-                ></youtube-player>
-            </div>
-            <div class="col-lg-4 col-md-6 mt-4">
-                <h5 class="mb-3">Артем Агабеков, «Фабрика Окон»</h5>
-                <youtube-player 
-                    video-id="WWzZ19ZCCE8"
-                    bg-img="/img/pages/videoreviews/videoreview_05.jpg"
-                ></youtube-player>
-            </div>
-            <div class="col-lg-4 col-md-6 mt-4">
-                <h5 class="mb-3">Андрей Черкасов, «Миг Плюс»</h5>
-                <youtube-player 
-                    video-id="MS8oCP5dIjc"
-                    bg-img="/img/pages/videoreviews/videoreview_06.jpg"
-                ></youtube-player>
-            </div>
-        </div>
+        
+        @include('pages.page.partials.videoreviews')
+
         @foreach ($reviews as $key => $review)
             <div class="row mt-5">
                 <div class="col">
